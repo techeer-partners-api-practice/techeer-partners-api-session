462c3aeb7b43af37e46516ac38afb198e5c486ac
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-11-09 01:58:02 +0900

    initial setting

---
+++ src/main/java/com/example/techeer_partners_api_session/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.techeer_partners_api_session.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소 (React 예시)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키 등 인증 정보 허용
+    }
+}
